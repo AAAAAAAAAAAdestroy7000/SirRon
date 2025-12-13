281aa2f0bc4ab050d8c2faf73a60b7e8ea30043a
AAAAAAAAAAAdestroy7000 <leagueof1v1@gmail.com>
2025-12-13 16:38:55 +0800

    Add files via upload
    initial

---
+++ WWW/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 text-end p-3">
+            <a href="login.php" class="btn btn-primary">Login</a>
+            <a href="register.php" class="btn btn-secondary">Sign Up</a>
+        </div>
+    </div>
+</div>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <form method="GET">
+                <input type="text" class="form-control" placeholder="Search places">
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
